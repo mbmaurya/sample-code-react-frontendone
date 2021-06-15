4a92f5352e8e4c9ec0d21253b2bed39bad6a4d2a
LAPTOP-9T1INISP\sushanth <sushanthchetipally09@gmail.com>
2021-06-15 19:26:10 +0530

    added scss files, images, home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+function Home() {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    );
+}
+
+export default Home;
